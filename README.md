0033d3e518345c45c0db87896b0280ee5a4e40f2
fire1ce <stas@3os.org>
2022-02-04 19:02:17 +0200

    script rewrite

---
+++ README.md
@@ -0,0 +1,111 @@
+# DDNS Cloudflare PowerShell Script
+
+[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/fire1ce/3os.org/tree/master/src)
+[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://mit-license.org/)
+
+*   DDNS Cloudflare PowerShell script for __Windows__.
+*   Choose any source IP address to update  __external__ or __internal__  _(WAN/LAN)_.
+*   For multiply lan interfaces like Wifi, Docker Networks and Bridges the script will automatically detects the primary Interface by priority.
+*   Cloudflare's options proxy and TTL configurable via the parameters.
+*   Optional Telegram Notifications
+
+## Requirements
+
+*   Cloudflare [api-token](https://dash.cloudflare.com/profile/api-tokens) with ZONE-DNS-EDIT Permissions
+*   DNS Record must be pre created (api-token should only edit dns records)
+*   Enabled running unsigned PowerShell
+
+Start Windows PowerShell with the "Run as Administrator" option and execute this command
+
+```powershell
+set-executionpolicy remotesigned
+```
+
+Choose __A__ for __all__
+
+## Installation
+
+[Download the DDNS-Cloudflare-PowerShell zip file](https://github.com/fire1ce/DDNS-Cloudflare-PowerShell/archive/refs/heads/main.zip) & Unzip,
+rename to folder to _DDNS-Cloudflare-PowerShell_ place in a directory of your choosing
+
+## Config Parameters
+
+Update the config parameters at updateDNS.ps1 by editing the file
+
+| __Option__                | __Example__      | __Description__                                           |
+| ------------------------- | ---------------- | --------------------------------------------------------- |
+| what_ip                   | internal         | Which IP should be used for the record: internal/external |
+| dns_record                | ddns.example.com | DNS __A__ record which will be updated                    |
+| cloudflare_zone_api_token | ChangeMe         | Cloudflare API Token __KEEP IT PRIVET!!!!__               |
+| zoneid                    | ChangeMe         | Cloudflare's Zone ID                                      |
+| proxied                   | false            | Use Cloudflare proxy on dns record true/false             |
+| ttl                       | 120              | 120-7200 in seconds or 1 for Auto                         |
+
+### Optional Notifications Parameters
+
+| __Option__             | __Example__ | __Description__                   |
+| ---------------------- | ----------- | --------------------------------- |
+| notify_me_telegram     | yes         | Use Telegram notifications yes/no |
+| telegram_chat_id       | ChangeMe    | Chat ID of the bot                |
+| telegtam_bot_API_Token | ChangeMe    | Telegtam's Bot API Token          |
+
+## Running The Script
+
+Right click on _update-cloudflare-dns_conf.ps1_ click __Run with PowerShell__
+
+## Automation With Windows Task Scheduler
+
+Run At boot Example with the updateDNS.ps1 C:\updateDNS.ps1:
+
+* Open Task Scheduler
+* Action -> Crate Task
+* __General Menu__
+    * Name: updateDNS
+    * Run whether user is logged on or not
+* __Trigger__
+    * New...
+    * Begin the task: At startup
+    * Delay Task for: 1 minute
+    * Enabled
+* __Actions__
+    * New...
+    * Action: Start a Program
+    * Program/script: _C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe_
+    * Add arguments (optional): _-File C:\DDNS-Cloudflare-PowerShell\update-cloudflare-dns.ps1_
+    * ok
+    * Enter your user's password when prompted
+
+## Logs
+
+This Script will create a log file with __only__ the last run information  
+Log file will be located as same directory as _update-cloudflare-dns.ps1_
+
+Log file name:
+
+```bash
+update-cloudflare-dns.log
+```
+
+## License
+
+### MIT License
+
+Copyright© 3os.org @2020
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
---
+++ update-cloudflare-dns.ps1
@@ -0,0 +1,157 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+### updateDNS.log file of the last run for debug
+$File_LOG = "$PSScriptRoot\update-cloudflare-dns.log"
+$FileName = "update-cloudflare-dns.log"
+
+if (!(Test-Path $File_LOG)) {
+  New-Item -ItemType File -Path $PSScriptRoot -Name ($FileName) | Out-Null
+}
+
+Clear-Content "update-cloudflare-dns.log"
+$DATE = Get-Date -UFormat "%Y/%m/%d %H:%M:%S"
+Write-Output "==> $DATE" | Tee-Object $File_LOG -Append
+
+### Load config file
+Try {
+  . $PSScriptRoot\update-cloudflare-dns_conf.ps1 
+}
+Catch {
+  Write-Output "==> Error! Missing update-cloudflare-dns_conf.ps1 or invalid syntax" | Tee-Object $File_LOG -Append
+  Exit
+}
+
+### Check validity of "ttl" parameter
+if (( $ttl -lt 120 ) -or ($ttl -gt 7200 ) -and ( $ttl -ne 1 )) {
+  Write-Output 'Error! ttl out of range (120-7200) or not set to 1' | Tee-Object $File_LOG -Append
+  Exit
+}
+
+### Check validity of "proxied" parameter
+if (!([string]$proxied) -or ($proxied.GetType().Name.Trim() -ne "Boolean")) {
+  Write-Output 'Error! Incorrect "proxied" parameter choose "$true" or "$false" ' | Tee-Object $File_LOG -Append
+  Exit
+}
+
+
+### Check validity of "what_ip" parameter
+if ( ($what_ip -ne "external") -and ($what_ip -ne "internal")) {
+  Write-Output 'Error! Incorrect "what_ip" parameter choose "external" or "internal"' | Tee-Object $File_LOG -Append
+  Exit
+}
+
+### Check if set to internal ip and proxy
+if (($what_ip -eq "internal") -and ($proxied)) {
+  Write-Output 'Error! Internal IP cannot be Proxied' | Tee-Object $File_LOG -Append
+  Exit
+}
+
+### Get External ip from https://checkip.amazonaws.com
+if ($what_ip -eq 'external') {
+  $ip = (Invoke-RestMethod -Uri "https://checkip.amazonaws.com" -TimeoutSec 10).Trim()
+  if (!([bool]$ip)) {
+    Write-Output "Error! Can't get external ip from https://checkip.amazonaws.com" | Tee-Object $File_LOG -Append
+    Exit
+  }
+  Write-Output "==> External IP is: $ip" | Tee-Object $File_LOG -Append
+}
+
+### Get Internal ip from primary interface
+if ($what_ip -eq 'internal') {
+  $default_interface = (Get-NetIPInterface -AddressFamily IPv4 -ConnectionState Connected | Sort-Object -Property InterfaceMetric | Select-Object -First 1).ifIndex
+  $ip = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $default_interface).IPAddress.Trim()
+  if (!([bool]$ip) -or ($ip -eq "127.0.0.1")) {
+    Write-Output "==>Error! Can't get internal ip address" | Tee-Object $File_LOG -Append
+    Exit
+  }
+  Write-Output "==> Internal IP is $ip" | Tee-Object $File_LOG -Append
+}
+
+### Get IP address of DNS record from 1.1.1.1 DNS server when proxied is "false"
+if ($proxied -eq $false) {
+  $dns_record_ip = (Resolve-DnsName -Name $dns_record -Server 1.1.1.1 -Type A | Select-Object -First 1).IPAddress.Trim()
+  if (![bool]$dns_record_ip) {
+    Write-Output "Error! Can't resolve the ${dns_record} via 1.1.1.1 DNS server" | Tee-Object $File_LOG -Append
+    Exit
+  }
+  $is_proxed = $proxied
+}
+
+### Get the dns record id and current proxy status from cloudflare's api when proxied is "true"
+if ($proxied -eq $true) {
+  $dns_record_info = @{
+    Uri     = "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?name=$dns_record"
+    Headers = @{"Authorization" = "Bearer $cloudflare_zone_api_token"; "Content-Type" = "application/json" }
+  }
+  
+  $response = Invoke-RestMethod @dns_record_info
+  if ($response.success -ne "True") {
+    Write-Output "Error! Can't get dns record info from cloudflare's api" | Tee-Object $File_LOG -Append
+  }
+  $is_proxed = $response.result.proxied
+  $dns_record_ip = $response.result.content.Trim()
+}
+
+
+### Check if ip or proxy have changed
+if (($dns_record_ip -eq $ip) -and ($is_proxed -eq $proxied)) {
+  Write-Output "==> DNS record IP of $dns_record is $dns_record_ip, no changes needed. Exiting..." | Tee-Object $File_LOG -Append
+  Exit
+}
+
+Write-Output "==> DNS record of $dns_record is: $dns_record_ip. Trying to update..." | Tee-Object $File_LOG -Append
+
+### Get the dns record information from cloudflare's api
+$cloudflare_record_info = @{
+  Uri     = "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?name=$dns_record"
+  Headers = @{"Authorization" = "Bearer $cloudflare_zone_api_token"; "Content-Type" = "application/json" }
+}
+
+$cloudflare_record_info_resposne = Invoke-RestMethod @cloudflare_record_info
+if ($cloudflare_record_info_resposne.success -ne "True") {
+  Write-Output "Error! Can't get $dns_record record inforamiton from cloudflare API" | Tee-Object $File_LOG -Append
+  Exit
+}
+
+### Get the dns record id from response
+$dns_record_id = $cloudflare_record_info_resposne.result.id.Trim()
+
+### Push new dns record information to cloudflare's api
+$update_dns_record = @{
+  Uri     = "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dns_record_id"
+  Method  = 'PUT'
+  Headers = @{"Authorization" = "Bearer $cloudflare_zone_api_token"; "Content-Type" = "application/json" }
+  Body    = @{
+    "type"    = "A"
+    "name"    = $dns_record
+    "content" = $ip
+    "ttl"     = $ttl
+    "proxied" = $proxied
+  } | ConvertTo-Json
+}
+
+$update_dns_record_response = Invoke-RestMethod @update_dns_record
+if ($update_dns_record_response.success -ne "True") {
+  Write-Output "Error! Update Failed" | Tee-Object $File_LOG -Append
+  Exit
+}
+
+Write-Output "==> Success!" | Tee-Object $File_LOG -Append
+Write-Output "==> $dns_record DNS Record Updated To: $ip, ttl: $ttl, proxied: $proxied" | Tee-Object $File_LOG -Append
+
+
+if ($notify_me_telegram -eq "no") {
+  Exit
+}
+
+if ($notify_me_telegram -eq "yes") {
+  $telegram_notification = @{
+    Uri    = "https://api.telegram.org/bot$telegram_bot_API_Token/sendMessage?chat_id=$telegram_chat_id&text=$dns_record DNS Record Updated To: $ip"
+    Method = 'GET'
+  }
+  $telegram_notification_response = Invoke-RestMethod @telegram_notification
+  if ($telegram_notification_response.ok -ne "True") {
+    Write-Output "Error! Telegram notification failed" | Tee-Object $File_LOG -Append
+    Exit
+  }
+}
