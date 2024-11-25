25a6fd008fd302f3835ecb604cdc5fd88f61c14c
Schwi <39186981+cyb233@users.noreply.github.com>
2024-11-25 20:20:20 +0800

    增加IPv6相关配置

--- update-cloudflare-dns_conf.ps1
+++ update-cloudflare-dns_conf.ps1
@@ -1,20 +1,36 @@
-﻿##### Config
+##### Config
+
+## update IPv4 or IPv6 or Both (v4/v6/all)
+$ip_version = "v4"
 
 ## Which IP should be used for the record: internal/external
 ## Internal interface will be chosen automaticly as a primary default interface
-$what_ip = "internal"
-## DNS A record to be updated
-$dns_record = "ddns.example.com"
+$what_ip_v4 = "internal"
+## DNS A/AAAA record to be updated
+$dns_record_v4 = "ChangeMe"
+## 60-7200 in seconds or 1 for Auto
+$ttl_v4 = 60
+## DNS record comment
+$comment_v4 = ""
+## Use Cloudflare proxy on dns record true/false
+$proxied_v4 = $false
+
+## Which IP should be used for the record: internal/external
+## Internal interface will be chosen automaticly as a primary default interface
+$what_ip_v6 = "internal"
+## DNS A/AAAA record to be updated
+$dns_record_v6 = "ChangeMe"
+## 60-7200 in seconds or 1 for Auto
+$ttl_v6 = 60
+## DNS record comment
+$comment_v6 = ""
+## Use Cloudflare proxy on dns record true/false
+$proxied_v6 = $false
+
 ## Cloudflare's Zone ID
 $zoneid = "ChangeMe"
 ## Cloudflare Zone API Token
 $cloudflare_zone_api_token = "ChangeMe"
-## Use Cloudflare proxy on dns record true/false
-$proxied = $false
-## 120-7200 in seconds or 1 for Auto
-$ttl = 120
-## DNS record comment
-$comment = ""
 
 ## Telegram Notifications yes/no (only sent if DNS is updated)
 $notify_me_telegram = "no"
@@ -30,5 +46,5 @@ $discord_webhook_URL = "ChangeMe"
 
 ## Notifications with Proxy yes/no
 $notify_with_proxy = "no"
-## HTTP Proxy URL
-$notify_proxy_URL = "ChangeMe"
+## Proxy URL (e.g. http://127.0.0.1:7890)
+$notify_proxy_URL = ""
